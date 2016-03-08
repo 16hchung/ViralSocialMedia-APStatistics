9c63c9e7f9beb575d89af1ec748206442401bb09
Heejung Chung <16hchung@castilleja.org>
2016-03-08 09:38:46 -0800

    Added more first steps (allow you to access data)

--- testInstagram.py
+++ testInstagram.py
@@ -1,2 +1,12 @@
-# GETTING AN ACCESS TOKEN:
-# https://instagram.com/oauth/authorize/?client_id=YOURCLIENTIDHERE&amp;redirect_uri=HTTP://YOURREDIRECTURLHERE.COM&amp;response_type=token
+'''
+FIRST STEPS:
+
+Access Token:
+1. enter link (https://instagram.com/oauth/authorize/?client_id=YOURCLIENTIDHERE&amp;redirect_uri=HTTP://YOURREDIRECTURLHERE.COM&amp;response_type=token) into browser
+2. click authorize -> will redirect to git repo
+3. go to end of redirect url -> string of char's after the "=" is the access token
+4. DO NOT UPLOAD THE ACCESS TOKEN TO GIT
+
+Increase scope:
+1. https://api.instagram.com/oauth/authorize/?client_id=CLIENTID&redirect_uri=REDIRECT-URI&response_type=code&scope=public_content
+'''
