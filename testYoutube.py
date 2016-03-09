07b7d4cf9cc9bef42212898fc45aab7fa65b7e72
Heejung Chung <16hchung@castilleja.org>
2016-03-09 15:01:25 -0800

    New directory for background paper .jmp files. testYoutube file edited more

--- testYoutube.py
+++ testYoutube.py
@@ -1,10 +1,9 @@
-#Source: https://developers.google.com/youtube/v3/code_samples/python#search_by_keyword
+#Original: https://developers.google.com/youtube/v3/code_samples/python#search_by_keyword
 
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 import json
-
 
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
 # tab of https://cloud.google.com/console
@@ -12,21 +11,22 @@ import json
 DEVELOPER_KEY = "INSERT KEY"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
+YoutubeService = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
-def youtube_search(options):
-  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    developerKey=DEVELOPER_KEY)
-
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
-  search_response = youtube.search().list(
+def constructRequest(options):
+  # Call the search.list method to retrieve results matching the specified query term.
+  # search().list() reference: https://goo.gl/2PQauI
+  search_response = YoutubeService.search().list(
     q=options.q,
     part="id,snippet",
     maxResults=options.max_results
   ).execute()
   
-  print(search_response)
+  return search_response
 
+def sampleSearchFromDocumentation(options):
+  search_response = constructRequest(options)
+  
   videos = []
   channels = []
   playlists = []
@@ -47,14 +47,22 @@ def youtube_search(options):
   print "Videos:\n", "\n".join(videos), "\n"
   print "Channels:\n", "\n".join(channels), "\n"
   print "Playlists:\n", "\n".join(playlists), "\n"
+  
+def newVideoTimeSeries(options):
+  vidCounts = []
+  search_response = constructRequest(options)
+  
+  
 
 
-#if __name__ == "__main__":
+
+
+
 argparser.add_argument("--q", help="Search term", default="icebucket")
 argparser.add_argument("--max-results", help="Max results", default=25)
 args = argparser.parse_args()
 
 try:
-  youtube_search(args)
+  sampleSearchFromDocumentation(args)
 except HttpError, e:
   print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
